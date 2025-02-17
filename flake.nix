cc3652e0b8f324f628176fc5b7bddca4319c5638
Hank Hogan <ysh2291939848@outlook.com>
2025-02-17 21:03:55 +0800

    setup nix env

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        name = "puzzel";
+        src = ./.;
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        packages.default = derivation {
+          inherit system name src;
+          builder = with pkgs; "${bash}/bin/bash";
+          args = ["-c" "cabal build"];
+        };
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            haskell.compiler.ghc9101
+            haskell.packages.ghc9101.haskell-language-server
+            clang
+            clang-tools
+            cabal-install
+          ];
+          shellHook = ''
+            export SHELL=$(which zsh)
+            exec zsh
+          '';
+        };
+      }
+    );
+}
