194a871dcf74a73dc62c65cbf09d3522fb800465
hank <ysh2291939848@outlook.com>
2025-06-09 19:06:33 +0800

    algebraic effect using delimited continuation

--- src/Delimited.hs
+++ src/Delimited.hs
@@ -8,7 +8,7 @@ import Control.Exception qualified as E
 import Control.Exception.Base (NoMatchingContinuationPrompt (..))
 import Data.Either
 import Data.Foldable (for_)
-import Data.Functor (void)
+import Data.Functor (void, ($>))
 import Data.Functor.Sum (Sum (..))
 import Data.Maybe (fromMaybe, maybe)
 import GHC.Exts (PromptTag#, control0#, newPromptTag#, prompt#)
@@ -75,8 +75,7 @@ catch f onThrow = do
   handle tag $ f tag
   where
     handle tag action = do
-      next <- prompt tag (Pure <$> action)
-      case next of
+      prompt tag (Pure <$> action) >>= \case
         Op (Throw e) -> onThrow e
         Pure a -> pure a
 
@@ -162,6 +161,144 @@ listInput l f = do
           | otherwise -> handle exc tag [] $ continue $ throw exc ()
         Pure a -> pure $ Just a
 
+-- Feed the output of one computation into the input of the other. Terminate whenever one side terminates, discarding the other.
+connect :: (Out x % a -> Mom a) -> (In x % a -> Mom a) -> Mom a
+connect producer consumer = do
+  out <- newPromptTag
+  inp <- newPromptTag
+  handleI out inp (Pure <$> producer out) (Pure <$> consumer inp)
+  where
+    handleI out inp produce consume = do
+      prompt inp consume >>= \case
+        Op (Input continue) -> handleO out inp produce continue
+        Pure a -> pure a
+    handleO out inp produce consume = do
+      prompt out produce >>= \case
+        Op (Output o continue) -> handleI out inp (continue $ pure ()) (consume $ pure o)
+        Pure a -> pure a
+
+-- Text output can be printed to stdout
+printOutput :: (Out String % () -> Mom ()) -> IO ()
+printOutput f = momToIO do
+  tag <- newPromptTag
+  handle tag (Pure <$> f tag)
+  where
+    handle out action = do
+      prompt out action >>= \case
+        Op (Output o continue) -> pure $ print o *> momToIO (handle out (continue $ pure ()))
+        Pure () -> pure $ pure ()
+    momToIO = fromMaybe (pure ()) . run
+
+-- we can forward text input from stdin
+readInput :: (In String % () -> Mom ()) -> IO ()
+readInput f = momToIO do
+  tag <- newPromptTag
+  handle tag (Pure <$> f tag)
+  where
+    handle inp action = do
+      prompt inp action >>= \case
+        Op (Input continue) -> pure do
+          i <- getLine
+          momToIO (handle inp (continue $ pure i))
+    momToIO = fromMaybe (pure ()) . run
+
+-- State
+data State s a
+  = Get (Mom s -> Mom a)
+  | Put s (Mom () -> Mom a)
+
+get :: State s % r -> Mom s
+get tag = control0 tag $ pure . Op . Get
+
+put :: State s % r -> s -> Mom ()
+put tag s = control0 tag $ pure . Op . Put s
+
+runState :: s -> (State s % a -> Mom a) -> Mom (s, a)
+runState s0 f = do
+  tag <- newPromptTag
+  handle tag s0 (Pure <$> f tag)
+  where
+    handle tag s action = do
+      prompt tag action >>= \case
+        Op (Get continue) -> handle tag s (continue $ pure s)
+        Op (Put s continue) -> handle tag s (continue $ pure ())
+        Pure a -> pure (s, a)
+
+incr :: State Int % r -> Mom ()
+incr st = do
+  n <- get st
+  put st $ n + 1
+
+logState :: Out String % r -> State Int % s -> Mom ()
+logState out st = do
+  n <- get st
+  log out $ show n
+
+incr2 :: Out String % r -> State Int % s -> Mom ()
+incr2 out st = do
+  incr st
+  logState out st
+  incr st
+  logState out st
+
+-- Nondeterminism
+data Nondet a where
+  Choose :: [x] -> (Mom x -> Mom a) -> Nondet a
+
+choose :: Nondet % r -> [x] -> Mom x
+choose tag xs = control0 tag $ pure . Op . Choose xs
+
+nameTheorems :: Nondet % r -> Mom String
+nameTheorems nd = do
+  name1 <- choose nd ["Church", "Curry"]
+  name2 <- choose nd ["Turing", "Howard"]
+  result <- choose nd ["thesis", "isomorphism"]
+  pure (name1 ++ "-" ++ name2 ++ " " ++ result)
+
+enumerate :: (Nondet % a -> Mom a) -> Out a % r -> Mom ()
+enumerate f out = do
+  tag <- newPromptTag
+  handle tag (Pure <$> f tag)
+  where
+    handle tag action = do
+      next <- prompt tag action
+      case next of
+        Op (Choose xs continue) -> for_ xs (handle tag . continue . pure)
+        Pure a -> output out a
+
+-- Concurrency
+data Conc a
+  = Fork (Mom ()) (Mom () -> Mom a)
+  | Yield (Mom () -> Mom a)
+
+fork :: Conc % r -> Mom () -> Mom ()
+fork tag thread = control0 tag $ pure . Op . Fork thread
+
+yield :: Conc % r -> Mom ()
+yield tag = control0 tag $ pure . Op . Yield
+
+simpleThread :: Out String % r -> Conc % s -> Int -> Mom ()
+simpleThread out conc n = do
+  log out $ show n
+  yield conc
+  log out $ show n
+  yield conc
+  log out $ show n
+  yield conc
+
+runConc :: (Conc % () -> Mom ()) -> Mom ()
+runConc f = do
+  tag <- newPromptTag
+  handle tag [Pure <$> f tag]
+  where
+    handle tag [] = pure ()
+    handle tag (t : ts) = do
+      prompt tag t >>= \case
+        Op (Yield continue) -> handle tag (ts ++ [continue $ pure ()])
+        Op (Fork th continue) -> handle tag (continue (pure ()) : ts ++ [Pure <$> th])
+        Pure () -> handle tag ts
+
+-- tests
 testFib :: IO ()
 testFib =
   assert $
@@ -187,3 +324,36 @@ testCsum =
       )
         == [1, 3, 6, 10, 15]
     )
+
+testState :: IO ()
+testState = do
+  assert ((collect \out -> runState 0 (incr2 out) $> ()) == ["1", "2"])
+  assert (run (discardedCollect $ runState 0 . incr2) == Just (2, ()))
+
+testEnumerate :: IO ()
+testEnumerate = do
+  assert
+    ( collect (enumerate nameTheorems)
+        == [ "Church-Turing thesis",
+             "Church-Turing isomorphism",
+             "Church-Howard thesis",
+             "Church-Howard isomorphism",
+             "Curry-Turing thesis",
+             "Curry-Turing isomorphism",
+             "Curry-Howard thesis",
+             "Curry-Howard isomorphism"
+           ]
+    )
+
+testInterleave :: IO ()
+testInterleave =
+  assert
+    ( collect (runConc . interleave123)
+        == ["1", "2", "3", "1", "2", "3", "1", "2", "3"]
+    )
+  where
+    interleave123 :: Out String % r -> Conc % s -> Mom ()
+    interleave123 out conc = do
+      fork conc (simpleThread out conc 1)
+      fork conc (simpleThread out conc 2)
+      fork conc (simpleThread out conc 3)
