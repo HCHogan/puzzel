eac6fbd1126931f59632a2d934b09805645d42f0
Hank Hogan <ysh2291939848@outlook.com>
2025-08-28 20:45:50 +0800

    update

--- src/Demo4.hs
+++ src/Demo4.hs
@@ -1,23 +1,35 @@
 module Demo4 where
 
 import Control.Concurrent (threadDelay)
+import Control.Monad
 import Control.Monad.State
+import Effectful
+import Effectful.State.Static.Local qualified as ES
 import System.Timeout (timeout)
 
-type MyApp = StateT Int IO
-
-flakyAction :: MyApp ()
-flakyAction = do
-  put 1
-  liftIO $ putStrLn "state updated to 1"
+flakyAction1 :: StateT Int IO ()
+flakyAction1 = do
+  modify (+ 1)
   liftIO $ threadDelay 1_000_000
-  liftIO $ putStrLn "complete"
 
-main :: IO ()
-main = do
-  finalStateNoTimeout <- runStateT flakyAction 0
-  putStrLn $ "result: " ++ show finalStateNoTimeout
+-- main :: IO ()
+-- main = void $ flip runStateT 0 $ do
+--   flakyAction1
+--   get >>= liftIO . print
+--
+--   withRunInIO $ \runInIO -> timeout 1000000 (runInIO flakyAction1)
+--   get >>= liftIO . print
 
-  timeout 1000000 (runStateT flakyAction 0) >>= \case
-    Nothing -> putStrLn "G"
-    Just a -> print a
+flakyAction2 :: (ES.State Int :> es, IOE :> es) => Eff es ()
+flakyAction2 = do
+  ES.modify (+ 1)
+  liftIO $ threadDelay 1_000_000
+
+main2 :: IO ()
+main2 = void $ runEff $ do
+  ES.runState 0 $ do
+    flakyAction2
+    ES.get >>= liftIO . print -- prints 1
+
+    withRunInIO $ \runInIO -> timeout 100_000 (runInIO flakyAction2)
+    ES.get >>= liftIO . print -- prints 2
